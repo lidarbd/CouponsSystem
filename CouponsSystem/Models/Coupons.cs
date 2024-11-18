3e19f017e946fd68e4d9e0aa74a21d37ac50a601
lidarbd <lidar210599@gmail.com>
2024-11-18 13:57:26 +0200

    Add AdminUsers and Coupons classes to represent MySQL database tables

---
+++ CouponsSystem/Models/Coupons.cs
@@ -0,0 +1,15 @@
+﻿namespace CouponsSystem.Models
+{
+    public class Coupons
+    {
+        public string Code { get; set; } = string.Empty;   // Primary Key
+        public string Description { get; set; } = string.Empty;
+        public int UserCreatorID { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public bool IsPercentage { get; set; }
+        public double Discount { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public bool IsMultipleDiscounts { get; set; }
+        public int? MaxUsageCount { get; set; }
+    }
+}
